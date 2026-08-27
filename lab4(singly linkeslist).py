69e2f3571d8eb06a771e85aa32f0cc822f64a553
Siva karthikeya <parasara.siva@gmail.com>
2026-08-27 14:48:31 +0530

    Update lab4(singly linkeslist).py

--- lab4(singly linkeslist).py
+++ lab4(singly linkeslist).py
@@ -69,4 +69,54 @@ my_list.insert_end(40)
 my_list.insert_at_index(2,50)
 print(my_list)
 my_list.cont()
+while True:
+    print("\n--- operations ---")
+    print("1. insert_begin")
+    print("2. insert_end")
+    print("3. insert_at_index")
+    print("4. del_begin")
+    print("5. del_at_end")
+    print("6. del_at_index")
+    print("7. print_list")
+    print("8. cont")
+    print("9. exit")
+    
+    choice = int(input("\nEnter choice: "))
+    
+    if choice == 1:
+        data = int(input("Enter data: "))
+        my_list.insert_begin(data)
+        
+    elif choice == 2:
+        data = int(input("Enter data: "))
+        my_list.insert_end(data)
+        
+    elif choice == 3:
+        index = int(input("Enter index: "))
+        data = int(input("Enter data: "))
+        my_list.insert_at_index(index, data)
+        
+    elif choice == 4:
+        my_list.del_begin()
+        
+    elif choice == 5:
+        my_list.del_at_end()
+        
+    elif choice == 6:
+        index = int(input("Enter index: "))
+        my_list.del_at_index(index)
+        
+    elif choice == 8:
+        my_list.cont()
+        
+    elif choice == 7:
+        my_list.print_list()
+        
+    elif choice == 9:
+        print("exiting...")
+        break
+        
+    else:
+        print("invalid choice")
+
 
